a93053aaa7ab646e34816fac0d1465cc29bf3d81
EJyoungc <nerst@live.com>
2023-12-07 17:02:11 +0200

    main

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        // $this->call(TableFooterSeeder::class);
-        // $this->call(UserTableSeeder::class);
+        $this->call(TableFooterSeeder::class);
+        $this->call(UserTableSeeder::class);
         $this->call(AboutTableSeeder::class);
     }
 }
