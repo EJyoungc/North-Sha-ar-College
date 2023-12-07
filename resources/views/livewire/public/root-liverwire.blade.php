2330d70adda965aa1c8f98a075ad9375c2d74a36
EJyoungc <nerst@live.com>
2023-12-07 22:00:32 +0200

    main

--- resources/views/livewire/public/root-liverwire.blade.php
+++ resources/views/livewire/public/root-liverwire.blade.php
@@ -1,63 +1,7 @@
 <div>
     {{-- If your happiness depends on money, you will never be happy with yourself. --}}
-    <section class="flexslider">
-
-        <ul class="slides">
-            @forelse ($hero as $item)
-                <li style="background-image: url({{ asset('assets/uploads/' . $item->image) }})" class="overlay">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8 col-md-offset-2">
-                                <div class="probootstrap-slider-text text-center">
-                                    <h1 class="probootstrap-heading probootstrap-animate">{{ $item->name }}</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            @empty
-
-                <li style="background-image: url(img/slider_1.jpg)" class="overlay">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8 col-md-offset-2">
-                                <div class="probootstrap-slider-text text-center">
-                                    <h1 class="probootstrap-heading probootstrap-animate">Your Bright Future is Our
-                                        Mission</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-
-                <li style="background-image: url(img/slider_2.jpg)" class="overlay">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8 col-md-offset-2">
-                                <div class="probootstrap-slider-text text-center">
-                                    <h1 class="probootstrap-heading probootstrap-animate">Education is Life</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                </li>
-                <li style="background-image: url(img/slider_3.jpg)" class="overlay">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-md-8 col-md-offset-2">
-                                <div class="probootstrap-slider-text text-center">
-                                    <h1 class="probootstrap-heading probootstrap-animate">Helping Each of Our Students
-                                        Fulfill the Potential</h1>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </li>
-            @endforelse
-
-        </ul>
-    </section>
+    
+    @livewire('public.components.root-hero-livewire')
 
     <section class="probootstrap-section probootstrap-section-colored">
         <div class="container">
