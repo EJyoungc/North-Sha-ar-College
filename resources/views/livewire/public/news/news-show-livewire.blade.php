f84cf1d96a7a96fac9a75aec9ab031290611a947
EJyoungc <nerst@live.com>
2023-12-09 07:17:54 +0200

    update

--- resources/views/livewire/public/news/news-show-livewire.blade.php
+++ resources/views/livewire/public/news/news-show-livewire.blade.php
@@ -52,7 +52,7 @@
           <div class="row">
             <div class="col-md-12">
               <h2 class="probootstrap-animate" data-animate-effect="fadeInRight">Get your admission now!</h2>
-              <a href="#" role="button" class="btn btn-primary btn-lg btn-ghost probootstrap-animate" data-animate-effect="fadeInLeft">Enroll</a>
+              <a href="{{ route('root.enroll') }}" role="button" class="btn btn-primary btn-lg btn-ghost probootstrap-animate" data-animate-effect="fadeInLeft">Enroll</a>
             </div>
           </div>
         </div>
