9a1b41f77909c172b3371ca73afd597c79596019
EJyoungc <nerst@live.com>
2023-12-15 13:30:15 +0200

    main

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,37 +1,42 @@
 @extends('layouts.auth')
 
 @section('content')
+    <div class="container ">
+        <div class="row d-flex justify-content-center ">
+            <div class="col-lg-6 pt-5 ">
+                <div class="card">
+                    <div class="card-body">
 
-<div class="card text-white bg-primary">
-  <div class="card-body">
 
+                        <p>
+                            {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+                        </p>
 
-    <p>
-        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
-    </p>
+                        <form method="POST" action="{{ route('password.confirm') }}">
+                            @csrf
 
-    <form method="POST" action="{{ route('password.confirm') }}">
-        @csrf
+                            <div class="form-group">
+                                <x-label for="password" value="{{ __('Password') }}" />
+                                <x-input id="password" class="form-control" type="password" name="password" required
+                                    autocomplete="current-password" autofocus />
+                            </div>
 
-        <div class="form-group" > 
-            <x-label for="password" value="{{ __('Password') }}" />
-            <x-input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" autofocus />
+                            <x-validation-errors class="mb-4" />
+
+                            <div class="flex justify-content-end mt-4">
+                                <button type="submit" class="btn btn-dark">
+                                    {{ __('Confirm') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <x-validation-errors class="mb-4" />
-        
-        <div class="flex justify-content-end mt-4">
-            <button type="submit" class="btn btn-dark">
-                {{ __('Confirm') }}
-            </button>
-        </div>
-    </form>
-  </div>
-</div>
-
+    </div>
 @endsection
 
-    {{-- <x-authentication-card>
+{{-- <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
