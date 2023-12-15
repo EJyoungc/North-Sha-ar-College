198dc43cdb85129ea13f1a7728fb3b6ba03bc0bb
EJyoungc <nerst@live.com>
2023-12-15 13:23:05 +0200

    main

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.auth')
 
 @section('content')
-    <div class="container">
-        <div class="row">
+    <div class="container ">
+        <div class="row d-flex justify-content-center " >
             <div class="col-lg-6">
                 <div class="card">
                     <div class="card-body">
