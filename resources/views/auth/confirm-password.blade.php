c81665e461c08aab03e1b41028714b64d83e65ea
EJyoungc <nerst@live.com>
2023-12-15 12:38:34 +0200

    main

--- resources/views/auth/confirm-password.blade.php
+++ resources/views/auth/confirm-password.blade.php
@@ -1,5 +1,35 @@
 <x-guest-layout>
-    <x-authentication-card>
+
+<div class="card text-white bg-primary">
+  <div class="card-body">
+
+
+    <p>
+        {{ __('This is a secure area of the application. Please confirm your password before continuing.') }}
+    </p>
+
+    <form method="POST" action="{{ route('password.confirm') }}">
+        @csrf
+
+        <div class="form-group" > 
+            <x-label for="password" value="{{ __('Password') }}" />
+            <x-input id="password" class="form-control" type="password" name="password" required autocomplete="current-password" autofocus />
+        </div>
+
+        <x-validation-errors class="mb-4" />
+        
+        <div class="flex justify-content-end mt-4">
+            <button type="submit" class="btn btn-dark">
+                {{ __('Confirm') }}
+            </button>
+        </div>
+    </form>
+  </div>
+</div>
+
+
+
+    {{-- <x-authentication-card>
         <x-slot name="logo">
             <x-authentication-card-logo />
         </x-slot>
@@ -10,19 +40,6 @@
 
         <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.confirm') }}">
-            @csrf
-
-            <div>
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" autofocus />
-            </div>
-
-            <div class="flex justify-end mt-4">
-                <x-button class="ml-4">
-                    {{ __('Confirm') }}
-                </x-button>
-            </div>
-        </form>
+       
     </x-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
