a44d2910b365c80065101ae48ce9b6128a294ef0
EJyoungc <nerst@live.com>
2023-12-15 13:09:58 +0200

    main

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -36,8 +36,8 @@
 
     <!-- Scripts -->
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
-    <link rel="stylesheet" href="{{ asset('build/assets/app-0958fbe4.css') }}">
-        <script src="{{ asset('build/assets/app-dbe23e4c.js') }}"></script>
+    {{-- <link rel="stylesheet" href="{{ asset('build/assets/app-0958fbe4.css') }}"> --}}
+        {{-- <script src="{{ asset('build/assets/app-dbe23e4c.js') }}"></script> --}}
     <!-- Styles -->
     @livewireStyles
 </head>
