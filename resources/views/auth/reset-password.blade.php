11ea87a53ca1151bc1a700b159e371a5f4487720
EJyoungc <nerst@live.com>
2023-12-15 13:37:00 +0200

    main

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,38 +1,47 @@
 @extends('layouts.auth')
 
 @section('content')
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+    <div class="container ">
+        <div class="row d-flex justify-content-center ">
+            <div class="col-lg-6 pt-5 ">
+                <div class="card">
+                    <div class="card-body">
 
-        <x-validation-errors class="mb-4" />
 
-        <form method="POST" action="{{ route('password.update') }}">
-            @csrf
+                        <x-validation-errors class="mb-4" />
 
-            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+                        <form method="POST" action="{{ route('password.update') }}">
+                            @csrf
 
-            <div class="block">
-                <x-label for="email" value="{{ __('Email') }}" />
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
+                            <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                            <div class="form-group">
+                                <x-label for="email" value="{{ __('Email') }}" />
+                                <x-input id="email" class="form-group" type="email" name="email" :value="old('email', $request->email)"
+                                    required autofocus autocomplete="username" />
+                            </div>
+
+                            <div class="form-group">
+                                <x-label for="password" value="{{ __('Password') }}" />
+                                <x-input id="password" class="form-control" type="password" name="password" required
+                                    autocomplete="new-password" />
+                            </div>
+
+                            <div class="form-group">
+                                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
+                                <x-input id="password_confirmation" class="form-control" type="password"
+                                    name="password_confirmation" required autocomplete="new-password" />
+                            </div>
+
+                            <div class="flex items-center justify-end mt-4">
+                                <button type="submit" class="btn btn-dark">
+                                    {{ __('Reset Password') }}
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-
-            <div class="mt-4">
-                <x-label for="password" value="{{ __('Password') }}" />
-                <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            </div>
-
-            <div class="mt-4">
-                <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-                <x-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" required autocomplete="new-password" />
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <x-button>
-                    {{ __('Reset Password') }}
-                </x-button>
-            </div>
-        </form>
-    </x-authentication-card>
+        </div>
+    </div>
 @endsection
--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,47 +1,52 @@
 @extends('layouts.auth')
 
 @section('content')
-    <x-authentication-card>
-        <x-slot name="logo">
-            <x-authentication-card-logo />
-        </x-slot>
+    <div class="container ">
+        <div class="row d-flex justify-content-center ">
+            <div class="col-lg-6 pt-5 ">
+                <div class="card">
+                    <div class="card-body">
 
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
-        </div>
+                        <div class="mb-4 text-sm text-gray-600">
+                            {{ __('Before continuing, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                        </div>
 
-        @if (session('status') == 'verification-link-sent')
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ __('A new verification link has been sent to the email address you provided in your profile settings.') }}
-            </div>
-        @endif
+                        @if (session('status') == 'verification-link-sent')
+                            <div class="mb-4 text-success">
+                                {{ __('A new verification link has been sent to the email address you provided in your profile settings.') }}
+                            </div>
+                        @endif
 
-        <div class="mt-4 flex items-center justify-between">
-            <form method="POST" action="{{ route('verification.send') }}">
-                @csrf
+                        <div class="mt-4 ">
+                            <form method="POST" action="{{ route('verification.send') }}">
+                                @csrf
 
-                <div>
-                    <x-button type="submit">
-                        {{ __('Resend Verification Email') }}
-                    </x-button>
+                                <div>
+                                    <button class="btn btn-dak" type="submit">
+                                        {{ __('Resend Verification Email') }}
+                                    </button>
+                                </div>
+                            </form>
+
+                            <div>
+                                <a href="{{ route('profile.show') }}"
+                                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                                    {{ __('Edit Profile') }}</a>
+
+                                <form method="POST" action="{{ route('logout') }}" class="inline">
+                                    @csrf
+
+                                    <button type="submit"
+                                        class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-2">
+                                        {{ __('Log Out') }}
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </form>
-
-            <div>
-                <a
-                    href="{{ route('profile.show') }}"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    {{ __('Edit Profile') }}</a>
-
-                <form method="POST" action="{{ route('logout') }}" class="inline">
-                    @csrf
-
-                    <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 ml-2">
-                        {{ __('Log Out') }}
-                    </button>
-                </form>
             </div>
         </div>
-    </x-authentication-card>
+    </div>
+
 @endsection
