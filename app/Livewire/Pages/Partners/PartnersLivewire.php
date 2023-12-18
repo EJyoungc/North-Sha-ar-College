6f401baf26c0786ff00f527948753fd53b6e60a5
EJyoungc <nerst@live.com>
2023-12-18 10:04:50 +0200

    main

--- app/Livewire/Pages/Partners/PartnersLivewire.php
+++ app/Livewire/Pages/Partners/PartnersLivewire.php
@@ -67,14 +67,14 @@ class PartnersLivewire extends Component
                 'image'=>'required|image',
             ]);
 
-            $p = new Partner();
+            $pn = new Partner();
             // Storage::disk('custom')->delete($this->id->image);
             $file =$this->image->store('partners','custom');
             $img = Image::make(public_path('assets/uploads/' . $file))->fit(400, 400);
             $img->save();
-            $p->name = $this->name;
-            $p->logo = $img;
-            $p->save();
+            $pn->name = $this->name;
+            $pn->logo = $img;
+            $pn->save();
             $this->cancel();
             $this->alert('success','successfull');
 
