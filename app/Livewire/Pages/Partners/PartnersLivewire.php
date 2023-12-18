7675c59e59de8051cc956fcab364c7f3d618240c
EJyoungc <nerst@live.com>
2023-12-18 11:23:46 +0200

    main

--- app/Livewire/Pages/Partners/PartnersLivewire.php
+++ app/Livewire/Pages/Partners/PartnersLivewire.php
@@ -8,25 +8,27 @@ use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 use Livewire\WithFileUploads;
 use Livewire\WithPagination;
+use Image;
 
 class PartnersLivewire extends Component
 {
     use WithPagination;
     use WithFileUploads;
     use LivewireAlert;
-    
-    public $modal =false;
+
+    public $modal = false;
     public $partner;
 
     public $name, $logo, $url, $description;
 
-    public function create(){
+    public function create()
+    {
         $this->modal = true;
-
     }
 
-    public function edit($data){
-        
+    public function edit($data)
+    {
+
         $this->partner = Partner::find($data);
         $this->name =  $this->partner->name;
         // $this->logo =  $this->partner->n;
@@ -35,51 +37,53 @@ class PartnersLivewire extends Component
         $this->modal  = true;
     }
 
-    public function store(){
+    public function store()
+    {
         $this->validate([
-            'name'=>'required|string',
-            'url'=>'sometimes|url',
-            'logo'=>'sometimes|image',
-            'description'=>'sometimes'
+            'name' => 'required|string',
+            'url' => 'sometimes|url',
+            'logo' => 'sometimes|image',
+            'description' => 'sometimes'
         ]);
 
-        $file = $this->logo->store('partners','custom');
-        
-        if($file != null){
-        $p = new Partner();
-        $p->name = $this->name;
-        $p->url =$this->url;
-        $p->logo = $file;
-        $p->save();
-        $this->cancel();
-        $this->alert('success','successfull!');
+        $file = $this->logo->store('partners', 'custom');
+        $img = Image::make(public_path('assets/uploads/' . $file))->fit(400, 400);
+        $img->save();
 
-        }else{
+        if ($file != null) {
             $p = new Partner();
-        $p->name = $this->name;
-        $p->url =$this->url;
-        $p->save();
-        $this->cancel();
-        $this->alert('success','successfull!');
+            $p->name = $this->name;
+            $p->url = $this->url;
+            $p->logo = $img;
+            $p->save();
+            $this->cancel();
+            $this->alert('success', 'successfull!');
+        } else {
+            $p = new Partner();
+            $p->name = $this->name;
+            $p->url = $this->url;
+            $p->save();
+            $this->cancel();
+            $this->alert('success', 'successfull!');
         }
-
-        
     }
 
-    public function delete($id){
+    public function delete($id)
+    {
 
-       $p = Partner::find($id);
-       Storage::disk('custom')->delete($p->logo);
-       $p->delete();
+        $p = Partner::find($id);
+        Storage::disk('custom')->delete($p->logo);
+        $p->delete();
     }
-    public function cancel(){
-        $this->reset(['name','url','logo','description','modal']);
+    public function cancel()
+    {
+        $this->reset(['name', 'url', 'logo', 'description', 'modal']);
     }
-    
+
     public function render()
     {
 
         $partners = Partner::paginate();
-        return view('livewire.pages.partners.partners-livewire')->with('partners',$partners);
+        return view('livewire.pages.partners.partners-livewire')->with('partners', $partners);
     }
 }
