4246c9c239eeee4ceb6134637865f6ac59ca0805
EJyoungc <nerst@live.com>
2023-12-18 12:32:17 +0200

    main

--- app/Livewire/Pages/Intake/IntakeCandidatesLivewire.php
+++ app/Livewire/Pages/Intake/IntakeCandidatesLivewire.php
@@ -44,7 +44,7 @@ class IntakeCandidatesLivewire extends Component
     public function approve_toggle($id = null)
     {
         $ic = IntakeCandidates::where('intake_id', $id)->first();
-        
+        dd($ic->status);
         if ($ic->status == "") {
             $ic->status = "disapproved";
             $ic->save();
