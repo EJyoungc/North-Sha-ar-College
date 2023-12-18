1862af7921b74f58732251ee6cbf641e0c371b3f
EJyoungc <nerst@live.com>
2023-12-18 03:38:57 +0200

    main

--- app/Livewire/Pages/Partners/PartnersLivewire.php
+++ app/Livewire/Pages/Partners/PartnersLivewire.php
@@ -2,12 +2,40 @@
 
 namespace App\Livewire\Pages\Partners;
 
+use App\Models\Partner;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class PartnersLivewire extends Component
 {
+
+    use LivewireAlert;
+    use WithFileUploads;
+    public $mobile = false;
+    public $image;
+    public $name;
+    public $id;
+
+
+
+
+    public function store()
+    {
+    }
+
+    public function delete()
+    {
+    }
+
+
+
+
+
+
     public function render()
     {
-        return view('livewire.pages.partners.partners-livewire');
+        $p = Partner::get();
+        return view('livewire.pages.partners.partners-livewire')->with('partners', $p);
     }
 }
