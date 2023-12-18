405f2fc6b3c77aabb214291b4e202852822f558d
EJyoungc <nerst@live.com>
2023-12-18 10:37:47 +0200

    main

--- app/Livewire/Public/Partners/PartnersLivewire.php
+++ app/Livewire/Public/Partners/PartnersLivewire.php
@@ -3,9 +3,13 @@
 namespace App\Livewire\Public\Partners;
 
 use Livewire\Component;
+use Livewire\Attributes\Layout;
+
 
 class PartnersLivewire extends Component
 {
+    #[Layout('layouts.root')] 
+
     public function render()
     {
         return view('livewire.public.partners.partners-livewire');
