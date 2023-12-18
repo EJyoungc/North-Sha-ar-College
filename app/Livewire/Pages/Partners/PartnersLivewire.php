f3e37ffa153f4c0d9f7ecd8fd201e1cbebc0a027
EJyoungc <nerst@live.com>
2023-12-18 09:50:48 +0200

    main

--- app/Livewire/Pages/Partners/PartnersLivewire.php
+++ app/Livewire/Pages/Partners/PartnersLivewire.php
@@ -3,10 +3,11 @@
 namespace App\Livewire\Pages\Partners;
 
 use App\Models\Partner;
+use Illuminate\Support\Facades\Storage;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 use Livewire\WithFileUploads;
-
+use Image;
 class PartnersLivewire extends Component
 {
 
@@ -16,16 +17,84 @@ class PartnersLivewire extends Component
     public $image;
     public $name;
     public $id;
+    
 
+    public function create($id = null){
+        if(!empty($id)){
+            $this->id = Partner::find($id);
+            $this->name = $this->id->name;
+            $this->image = $this->id->image;
+            $this->modal = true;
+        }else{
+            $this->modal = true;
+        }
 
+    }
 
 
     public function store()
     {
+        if(!empty($this->id->id)){
+            if(!empty($this->image)){
+                $this->validate([
+                    'name'=>'required',
+                    'image'=>'required|image',
+                ]);
+
+                Storage::disk('custom')->delete($this->id->image);
+                $file =$this->image->store('partners','custom');
+                $img = Image::make(public_path('assets/uploads/' . $file))->fit(400, 400);
+                $img->save();
+                $this->id->name = $this->name;
+                $this->id->image = $this->img;
+                $this->id->save();
+                $this->cancel();
+                $this->alert('success','update');
+
+            }else{
+
+                $this->id->name = $this->name;
+                $this->id->save();
+                $this->cancel();
+                $this->alert('success','update!');
+
+
+            }
+        }else{
+
+            $this->validate([
+                'name'=>'required',
+                'image'=>'required|image',
+            ]);
+
+            $p = new Partner();
+            Storage::disk('custom')->delete($this->id->image);
+            $file =$this->image->store('partners','custom');
+            $img = Image::make(public_path('assets/uploads/' . $file))->fit(400, 400);
+            $img->save();
+            $p->name = $this->name;
+            $p->image = $img;
+            $p->save();
+            $this->cancel();
+            $this->alert('success','successfull');
+
+        
+
+
+        }
     }
 
-    public function delete()
+
+    public function cancel(){
+        $this->reset(['name','modal','image',]);
+    }
+
+    public function delete($id)
     {
+       $p = Partner::find($id);
+       Storage::disk('custom')->delete($p->image);
+       $p->delete();
+       $this->alert('success','successfull');
     }
 
 
@@ -35,7 +104,7 @@ class PartnersLivewire extends Component
 
     public function render()
     {
-        $p = Partner::get();
-        return view('livewire.pages.partners.partners-livewire')->with('partners', $p);
+        $this->id = Partner::get();
+        return view('livewire.pages.partners.partners-livewire')->with('partners', $this->id);
     }
 }
