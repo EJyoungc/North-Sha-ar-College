5d05dbe4170b8cfaa4aaba60550c8bd4d35af5db
EJyoungc <nerst@live.com>
2024-09-17 11:37:10 +0200

    main

--- resources/views/livewire/public/include/nav-top-livewire.blade.php
+++ resources/views/livewire/public/include/nav-top-livewire.blade.php
@@ -47,7 +47,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="{{ route('root') }}" title="ProBootstrap:lorem">lorem</a> 
+                <a class="navbar-brand" href="{{ route('root') }}">lorem</a> 
             </div>
 
             <div id="navbar-collapse" class="navbar-collapse collapse">
